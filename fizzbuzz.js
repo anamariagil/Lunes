e1eaaf5ba55fd81035a44608bfecc85da697654a
anamariagil <mazoana701@gmail.com>
2021-08-31 12:06:54 -0500

    Funciones

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,7 +4,7 @@ de 5 por “Buzz”. Para los números que, al tiempo, son múltiplos de 3 y 5, 
 “FizzBuzz*/
 
 let i=1
-while (i<200) {
+while (i<=200) {
     if ((i%5 || i%3)==0){
         console.log("FizzBuzz");
     } else {
